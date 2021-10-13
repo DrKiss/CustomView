7016e28a96ca448b71f89aeabaaa7aebc1364fd9
Robert <robert.covu@gmail.com>
2021-10-12 22:46:09 -0400

    CustomView

--- CustomView/ViewController.swift
+++ CustomView/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segController : UISegmentedControl!
+    @IBOutlet weak var datePic : UIDatePicker!
+    @IBOutlet weak var imageHolder : UIImageView!
+    @IBOutlet weak var dateLable : UILabel!
+    @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var sliderValue : UILabel!
+    @IBOutlet weak var buttonItem : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   @IBAction func segControllerChange (_ sender:UISegmentedControl){
+        
+    switch segController.selectedSegmentIndex{
+    case 0: print("Selected 1")
+    case 1: print("Selected 2")
+    default: break
+    }
+    
+    
+    
+    }
+    @IBAction func sliderValueChang(_ sender:UISlider){
+        
+        let currentValue = Float (sender.value)
+        sliderValue.text = "\(currentValue)"
+    }
+    
+    @IBAction func  datePicker (_ sender:Any){
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let startDate = dateFormatter.string (from: datePic.date)
+        
+        dateLable.text = startDate
+    }
 }
 
